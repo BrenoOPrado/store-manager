3dea247e6b29bd9c8d1499057157958202afdc83
BrenoOPrado <breno.o.prado@hotmail.com>
2022-10-18 20:18:39 -0300

    Testes em desenvolvimento :turtle:

--- src/models/productsDB.js
+++ src/models/productsDB.js
@@ -1,17 +1,17 @@
 const conn = require('./db/connection');
 
-const findAll = async () => conn.execute('SELECT * FROM StoreManager.products ORDER BY id');
+const findAll = () => conn.execute('SELECT * FROM StoreManager.products ORDER BY id');
 
-const findById = async (id) => conn.execute('SELECT * FROM products WHERE id = ?', [id]);
+const findById = (id) => conn.execute('SELECT * FROM products WHERE id = ?', [id]);
 
-const insert = async (name) => conn.execute('INSERT INTO products (name) VALUES (?)', [name]);
+const insert = (name) => conn.execute('INSERT INTO products (name) VALUES (?)', [name]);
 
-const update = async ({ name, id }) => conn.execute(
+const update = ({ name, id }) => conn.execute(
   'UPDATE products SET name = ? WHERE id = ?',
   [name, id],
 );
 
-const remove = async (id) => conn.execute('DELETE FROM products WHERE id = ?', [id]);
+const remove = (id) => conn.execute('DELETE FROM products WHERE id = ?', [id]);
 
 module.exports = {
   findAll,
